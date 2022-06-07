bdcb178c8921c67846f5727cc798548685acddad
SDJMOCGSJY <2575848127@qq.com>
2022-06-07 21:42:47 +0800

    9_10_1

---
+++ 9_9_2_exit/9_9_2_exit.c
@@ -0,0 +1,20 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+
+int main(void) 
+{ 
+    printf("Hello World!"); 
+    switch (fork()) 
+    { 
+        case -1: perror("fork error"); 
+            exit(-1); 
+        case 0: 
+            /* 子进程 */ 
+            //exit(0); 
+            _exit(0); 
+        default: 
+            /* 父进程 */ 
+            exit(0); 
+    }
+}
